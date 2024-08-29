bdde4c1822666bc9134019f21f67f592af03eb51
muthuhariharan2001 <164756456+muthuhariharan2001@users.noreply.github.com>
2024-08-29 19:23:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import Login from './pages/login/Login';
+
+import Login from './pages/Login/Login';
 import Chat from './pages/Chat/Chat';
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate';
 import { ToastContainer, toast } from 'react-toastify';
